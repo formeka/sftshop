eba73a5d8531c9b67638617d01cfe84755d383bc
Mehdi Key <formk@kermed.fr>
2024-02-09 14:40:06 +0100

    Creation formulaire de cojntact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(Request $request,MailerInterface $mailer): Response
+    {
+        $formContact = $this->createForm(ContactType::class);
+        $formContact->handleRequest($request);
+
+        if ($formContact->isSubmitted() && $formContact->isValid()) :
+            $contactData = $formContact->getData();
+
+            $email = (new Email())
+                ->from('contact@ceppic.fr')
+                ->to($contactData['email'])
+                ->subject($contactData['sujet'])
+                ->text($contactData['message'])
+                ->html('<p>' . $contactData['message'] . '</p>');
+
+            $mailer->send($email);
+
+            return $this->redirectToRoute('app_homepage');
+
+        endif;
+
+
+        return $this->render('contact/index.html.twig', [
+            'formContact' => $formContact,
+        ]);
+    }
+}
